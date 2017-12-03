a5e0c07e702d1d5ea9771ea125ceb2753a80fd5b
Elliott Miller <eemiller1@crimson.ua.edu>
2017-12-03 17:23:49 -0600

    config file

---
+++ config.py
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#	https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+
+consumer_key = "PiSgO2Ozsap4AmZoUHMyNNm7r"
+consumer_secret = "X6PVxCxXKBfMW7NCNnHTvdDRJbux3NfP48lnIo8cBQRbNtMZU4"
+access_key = "34117915-s0S6HiFsqC68KZlnLEyiA4WYoK77brhJeSiGWpd1b"
+access_secret = "eiGVnQShGOoTnJajcIAGPlB4TLws4eHMRK1C3C66hcQjM"
